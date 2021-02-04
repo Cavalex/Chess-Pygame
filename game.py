b7340c549864079dfb15e31422b5a844dbd3d19b
Cavalex <mateusmpta@gmail.com>
2021-02-04 06:52:15 +0000

    checkmate functionality is complete

--- game.py
+++ game.py
@@ -11,11 +11,11 @@ WIDTH = 800
 DIMENSION = 8
 IMAGES = {}
 
-text0 = "You are in Check"
+text0 = ""
 text1 = "This is a Chess Game!"
 text2 = ("Player: white")
 text3 = ""
-text4 = "You are in Check"
+text4 = ""
 font = pygame.font.Font("fonts/LEMONMILK-Regular.otf", 30)
 textX = WIDTH + 30
 textY0 = 100
@@ -161,25 +161,40 @@ def willCollide(coords):
 
     return collided
 
-def getAllMovesWithPiece(piece, pieceCoords):
+def getAllLegalMovesWithPiece(piece, pieceCoords):
     moves = []
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             coords = (r,c)
-            if canMove(piece, (pieceCoords, (r,c)))[2]:
+            #print("(r,c) =",r,",",c,"and pieceCoords =", pieceCoords)
+            if isLegalMove(piece, (pieceCoords, (r,c))):
                 moves.append(coords)
     return moves
 
-def isKingUnderAttack():
-    kingCoords = getKingsPositions()[0] if playerTurn == -1 else getKingsPositions()[1]
-    print("\nPrints related to the king:")
+# this one returns a tuple with a coordinate with a piece and all the possible moves that piece can go to
+def getAllLegalMoves(player):
+    moves = []
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece[0].lower() == "b" and player == -1:
+                moves.append(((r,c),getAllLegalMovesWithPiece(piece, (r,c))))
+            elif piece[0].lower() == "w" and player == 1:
+                moves.append(((r,c),getAllLegalMovesWithPiece(piece, (r,c))))
+            else:
+                continue
+    return moves
+
+def isKingUnderAttack(player):
+    kingCoords = getKingsPositions()[0] if player == -1 else getKingsPositions()[1]
+    #print("\nPrints related to the king:")
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             coords = (r,c)
             pieceAtCoords = getPieceAtLocation(coords)
-            if pieceAtCoords == "--":
+            if pieceAtCoords == "--": #or pieceAtCoords[-1].lower() == "k":
                 continue
-            elif canMove(pieceAtCoords, (coords, kingCoords))[2]:
+            elif canMove(pieceAtCoords, (coords, kingCoords)):
                 return True
     return False
 
@@ -188,19 +203,17 @@ def movePiece(fst, snd, piece):
     changePieceAtLocation(fst, "--")
 
 def canMove(pieceC, coords):
-    blackKingPosition = getKingsPositions()[0]
-    whiteKingPosition = getKingsPositions()[1]
-
     fst = coords[0]
     snd = coords[1]
 
     pieceSnd = getPieceAtLocation(snd)
+
     piece = pieceC[-1].lower()
 
     collided = willCollide(coords)
-    ateBlackKing = True if not collided and pieceSnd.lower() == "bk" else False
-    ateWhiteKing = True if not collided and pieceSnd.lower() == "wk" else False
 
+    #ateBlackKing = True if not collided and pieceSnd.lower() == "bk" else False
+    #ateWhiteKing = True if not collided and pieceSnd.lower() == "wk" else False
     #print("black king was eaten, lol:", str(ateBlackKing) + "\n" + "white king was eaten, lol:", str(ateWhiteKing))
 
     #Movement of the pieces:
@@ -209,40 +222,40 @@ def canMove(pieceC, coords):
         if piece == "p":
             if pieceC == "bP":
                 if ((snd[0] - fst[0] == 1 and snd[1] == fst[1]) or (snd[0] == 3 and fst[0] == 1) and snd[1] == fst[1]) and pieceSnd == "--":
-                    return (ateBlackKing,ateWhiteKing ,True)
+                    return True
                 elif (snd[0] - fst[0] == 1 and abs(snd[1] - fst[1]) == 1) and pieceSnd != "--" and pieceSnd[0] != "b":
-                    return (ateBlackKing,ateWhiteKing ,True)
+                    return True
             elif pieceC == "wP":
                 if ((snd[0] - fst[0] == -1 and snd[1] == fst[1]) or (snd[0] == 4 and fst[0] == 6) and snd[1] == fst[1]) and pieceSnd == "--":
-                    return (ateBlackKing,ateWhiteKing ,True)
+                    return True
                 elif (snd[0] - fst[0] == -1 and abs(snd[1] - fst[1]) == 1) and pieceSnd != "--" and pieceSnd[0] != "w":
-                    return (ateBlackKing,ateWhiteKing ,True)
+                    return True
                 
         #Knights
         if piece == "n":
             if ((abs(snd[1] - fst[1]) == 2 and abs(snd[0] - fst[0]) == 1) or (abs(snd[0] - fst[0]) == 2 and abs(snd[1] - fst[1]) == 1)) and pieceC[0] != pieceSnd[0]:
-                return (ateBlackKing,ateWhiteKing ,True)
+                return True
         
         #Bishops
         if piece == "b":
             if (abs(snd[1] - fst[1]) == abs(snd[0] - fst[0])) and pieceC[0] != pieceSnd[0]:
-                return (ateBlackKing,ateWhiteKing ,True)
+                return True
         
         #Rooks
         if piece == "r":
             if ((snd[0] == fst[0]) or (snd[1] == fst[1])) and pieceC[0] != pieceSnd[0]:
-                return (ateBlackKing,ateWhiteKing ,True)
+                return True
         
         #Queens
         if piece == "q":
             if (((snd[0] == fst[0]) or (snd[1] == fst[1])) or (abs(snd[1] - fst[1]) == abs(snd[0] - fst[0]))) and pieceC[0] != pieceSnd[0]:
-                return (ateBlackKing,ateWhiteKing ,True)
+                return True
 
         #Kings
         if piece == "k":
             if pieceC[0] != pieceSnd[0] and ((abs(snd[0] - fst[0]) == 1 and snd[1] == fst[1]) or (snd[0] == fst[0] and abs(snd[1] - fst[1]) == 1) or (abs(snd[1] - fst[1]) == 1 and abs(snd[0] - fst[0]) == 1)):
-                    return (ateBlackKing,ateWhiteKing ,True)
-    return (ateBlackKing,ateWhiteKing ,False)
+                return True
+    return False
 
 def isLegalMove(pieceC, coords):
     
@@ -250,26 +263,39 @@ def isLegalMove(pieceC, coords):
     snd = coords[1]
 
     pieceSnd = getPieceAtLocation(snd)
-    piece = pieceC[-1].lower()
+
+    if pieceSnd[-1].lower() == "k":
+        return False
     
-    #return canMove(pieceC, coords)[2]
+    #return canMove(pieceC, coords)
 
     # make the new board and see if it is attacking the king
+    # then put it back on its original position
     movePiece(fst, snd, pieceC)
-    if isKingUnderAttack():
+    if isKingUnderAttack(playerTurn):
         movePiece(snd, fst, pieceC)
         changePieceAtLocation(snd, pieceSnd)
         return False
     else:
         movePiece(snd, fst, pieceC)
         changePieceAtLocation(snd, pieceSnd)
-        return canMove(pieceC, coords)[2]
+        return canMove(pieceC, coords)
+
+def isCheckMate(legalMoves):
+    for move in legalMoves:
+        if not move[1]: # check if is empty
+            continue
+        else:
+            return False
+    return True
 
 def updateBoard(clicks):
 
+    global text0
     global text1
     global text2
     global text3
+    global text4
 
     fstLocation = clicks[0]
     sndLocation = clicks[1]
@@ -279,21 +305,23 @@ def updateBoard(clicks):
     if not ((piece[0] == "w" and playerTurn == 1) or (piece[0] == "b" and playerTurn == -1)):
         text2 = "Not your turn!"
         text3 = "Try again!"
-        return False
 
-    #print("Is the king under attack?:", str(isKingUnderAttack()))
-    #print("Is this move legal?:",str(isLegalMove(piece, (fstLocation, sndLocation))))
+        print("Wrong player playing")
+        return False
 
     if not isLegalMove(piece, (fstLocation, sndLocation)):
         text2 = "Illegal Move!"
         text3 = "Try again!"
+
+        print("Illegal Move")
         return False
     else:
         playerToPlay = "white" if playerTurn == -1 else "black" # must be reversed
         text2 = ("Player: %s" % playerToPlay)
         text3 = ""
 
-    if not canMove(piece, (fstLocation, sndLocation))[2]:
+    if not canMove(piece, (fstLocation, sndLocation)):
+        print("can't move to location")
         return False
 
     # I want to make something that records moves in chess notation
@@ -301,7 +329,27 @@ def updateBoard(clicks):
     
     movePiece(fstLocation, sndLocation, piece)
 
+    text0 = "You are in Check!" if isKingUnderAttack(-1) else ""
+    text4 = "You are in Check!" if isKingUnderAttack(1) else ""
+
     return True
+
+def updateMateText(legalMoves):
+    global text0
+    global text4
+    mate = isCheckMate(legalMoves)
+    if mate:
+        text0 = "CheckMate!" if mate else ""
+        text4 = "CheckMate!" if mate else ""
+        return False
+
+# to be used only inside main()
+def printStats(legalMoves, playerClicks = ""):
+    print("\n################################")
+    print("Player:", playerTurn)
+    print("IsMate:",isCheckMate(legalMoves))
+    #print("LegalMoves:", legalMoves)
+    print("Clicks:",playerClicks)
 
 def main():
     global playerTurn
@@ -316,6 +364,9 @@ def main():
 
     squareSelected = ()
     playerClicks = [] #keep track of player clicks: 2 tuples [(6,4),(4,4)]
+
+    legalMoves = getAllLegalMoves(playerTurn)
+    printStats(legalMoves)
     
     while running:
         for event in pygame.event.get():
@@ -325,7 +376,7 @@ def main():
                 location = pygame.mouse.get_pos() #(x,y) do rato
                 col = location[0] // squareSize
                 row = location[1] // squareSize
-                if squareSelected == (row,col): #clicked the same square twice
+                if squareSelected == (row,col) or col >= 8 or row >= 8: #clicked the same square twice
                     squareSelected = ()
                     playerClicks = [] # clear playerCLicks
                 else:
@@ -335,17 +386,15 @@ def main():
                     squareSelected = ()
                     playerClicks = []
                 if len(playerClicks) == 2: # after 2nd click
-
-                    #print(getAllMovesWithPiece(getPieceAtLocation(playerClicks[0]), playerClicks[0]))
-                    
-                    print("\nPlayer:", playerTurn)
-                    print("Clicks:",playerClicks)
                     if not updateBoard(playerClicks): # if the piece cant move, then reset
                         squareSelected = ()
                         playerClicks = []
                     else:
-                        print("Jogou")
                         playerTurn = -playerTurn
+                        legalMoves = getAllLegalMoves(playerTurn)
+                        updateMateText(legalMoves)
+                        printStats(legalMoves, playerClicks)
+                        squareSelected = ()
                         playerClicks = []
 
         #screen.fill(pygame.Color("brown"))
